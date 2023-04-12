e2de7281d69f73daed0007a84cfc45c500a9393e
Øystein Riiser Gundersen <oysteinrg@gmail.com>
2023-04-12 17:02:11 +0200

    Initial GPT-4 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const items = [
+	{ name: 'Blå', price: 49 },
+	{ name: 'Gul', price: 35 },
+];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [counts, setCounts] = useState({});
+
+	useEffect(() => {
+		const savedCounts = localStorage.getItem('counts');
+		if (savedCounts) {
+			setCounts(JSON.parse(savedCounts));
+		}
+	}, []);
+
+	const updateCount = (item, delta) => {
+		const newCounts = {
+			...counts,
+			[item.name]: Math.max((counts[item.name] || 0) + delta, 0),
+		};
+		setCounts(newCounts);
+		localStorage.setItem('counts', JSON.stringify(newCounts));
+	};
+
+	const getTotal = () => {
+		return items.reduce(
+			(total, item) => total + (counts[item.name] || 0) * item.price,
+			0
+		);
+	};
+
+	return (
+		<div className="App">
+			<h1>Varer jeg skylder</h1>
+			<ul>
+				{items.map((item) => (
+					<li key={item.name}>
+						{item.name} ({item.price} kr) - Antall: {counts[item.name] || 0}
+						<button onClick={() => updateCount(item, 1)}>+</button>
+						<button onClick={() => updateCount(item, -1)}>-</button>
+					</li>
+				))}
+			</ul>
+			<h2>Totalbeløp: {getTotal()} kr</h2>
+		</div>
+	);
 }
 
 export default App;
